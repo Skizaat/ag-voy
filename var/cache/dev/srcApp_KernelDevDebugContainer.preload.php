29bee440b34dbeb46796b3a1d17e80bec2714b02
Skizaat <chloe.nquin@gmail.com>
2021-10-15 15:51:03 +0200

    change pages to display french text

--- var/cache/dev/srcApp_KernelDevDebugContainer.preload.php
+++ var/cache/dev/srcApp_KernelDevDebugContainer.preload.php
@@ -10,7 +10,7 @@ if (in_array(PHP_SAPI, ['cli', 'phpdbg'], true)) {
 }
 
 require dirname(__DIR__, 3).'/vendor/autoload.php';
-require __DIR__.'/Container8OSVUbx/srcApp_KernelDevDebugContainer.php';
+require __DIR__.'/ContainerUelDWYg/srcApp_KernelDevDebugContainer.php';
 
 $classes = [];
 $classes[] = 'Symfony\Bundle\FrameworkBundle\FrameworkBundle';
